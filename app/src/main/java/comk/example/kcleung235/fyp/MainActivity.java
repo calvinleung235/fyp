5dc1a48fe74fecd5d8fc6ebf164437d400d5f18f
KC Leung <neilnsum@gmail.com>
2018-01-20 16:52:16 +0800

    First commit

---
+++ app/src/main/java/comk/example/kcleung235/fyp/MainActivity.java
@@ -0,0 +1,57 @@
+package comk.example.kcleung235.fyp;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        OpenCVLoader.initDebug();
+        Mat rgbMat = new Mat();
+        Mat grayMat = new Mat();
+        Bitmap srcBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.book_img);
+        Bitmap grayBitmap = Bitmap.createBitmap(srcBitmap.getWidth(), srcBitmap.getHeight(), Bitmap.Config.RGB_565);
+        Utils.bitmapToMat(srcBitmap, rgbMat);//convert original bitmap to Mat, R G B.
+        Imgproc.cvtColor(rgbMat, grayMat, Imgproc.COLOR_RGB2GRAY);//rgbMat to gray grayMat
+        Utils.matToBitmap(grayMat, grayBitmap); //convert mat to bitmap
+
+//        ImageView img = (ImageView) findViewById(R.id.img);
+//        img.setImageBitmap(grayBitmap);
+        detectEdges(srcBitmap);
+
+    }
+
+    private void detectEdges(Bitmap bitmap) {
+        Mat rgba = new Mat();
+        Utils.bitmapToMat(bitmap, rgba);
+
+        Mat edges = new Mat(rgba.size(), CvType.CV_8UC1);
+        Imgproc.cvtColor(rgba, edges, Imgproc.COLOR_RGB2GRAY, 4);
+        Imgproc.Canny(edges, edges, 80, 100);
+
+        Bitmap resultBitmap = Bitmap.createBitmap(edges.cols(), edges.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(edges, resultBitmap);
+        ImageView img = (ImageView) findViewById(R.id.img);
+        img.setImageBitmap(resultBitmap);
+
+//        // Don't do that at home or work it's for visualization purpose.
+//        BitmapHelper.showBitmap(this, bitmap, imageView);
+//        BitmapHelper.showBitmap(this, resultBitmap, detectEdgesImageView);
+    }
+
+}
